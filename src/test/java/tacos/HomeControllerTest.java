b329ec84ab412046e09d6d72bbb87e5ab178bc34
jhmoon <dothebestmoon1016@gmail.com>
2023-06-28 18:30:36 +0900

    init

---
+++ src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package tacos;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; // MockMvc
+
+    @Test
+    public void testHomePages() throws Exception {
+        mockMvc.perform(get("/"))
+
+                .andExpect(status().isOk())
+
+                .andExpect(view().name("home"))
+
+                .andExpect(content().string(
+                        containsString("Welcome to...")));
+
+    }
+}
